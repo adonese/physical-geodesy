ec02fe59013d50545271cd9a6561e616ee50f5f9
adonese <mmbusif@student.uofk.edu>
2017-02-15 23:33:51 +0300

    adding week-5 contents

---
+++ Week-5/assignment-5/legendre_full.m
@@ -0,0 +1,6 @@
+function p = legendre_full(n , m, phi)
+   # This function computes the Legendre polynomials.
+  # n: is the degree,
+  # m: is the order
+  # phi: is the latitude.
+  your code goes here...
---
+++ Week-5/assignment-5/legendre_polynomials.m
@@ -0,0 +1,7 @@
+function p = legendre_polynomials(n, phi)
+  # This function computes the Legendre polynomials for m = 0.
+  # n: is the degree,
+  # phi: is the latitude.
+  
+  your code goes here...
+  
